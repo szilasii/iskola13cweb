eeb85958bd8dafa8b653ca544ea0b2e4e7acda20
szilasii <szilasii78@gmail.com>
2022-11-29 12:57:48 +0100

    órai feladat 2022.11.29

---
+++ 2022_11_29/script.js
@@ -0,0 +1,19 @@
+const tanulo = {nev: "Maci Laci",
+                cime: "Yellowstone",
+                kedvenc: "Méz",
+                kiirLog: function() {
+                    console.log("A tanulo neve: ",this.nev)
+                    console.log("A tanulo címe: ",this.cime)
+                    console.log("A tanulo kedvenc étele: ", this.kedvenc) 
+                },
+                kiirInnerHtml: function() {
+                    return "A tanulo neve: " + this.nev +"<br>" +
+                    "A tanulo címe: " + this.cime + "<br>" +
+                    "A tanulo kedvenc étele: " + this.kedvenc
+                }
+            }
+
+tanulo.kiirLog()
+
+tanulo.kedvenc ="Mézes puszedli"
+console.log("A tanulo kedvenc étele: ", tanulo["kedvenc"])
