5cb42c227f4804345c1a8394f9ae8589062409ec
Szilasi István <szilasi.istvan@bolyai.local>
2023-01-10 13:08:09 +0100

    commit products

--- 2023_01_01/app.js
+++ 2023_01_01/app.js
@@ -7,9 +7,33 @@ vegburger.addEventListener('click',(event)=>{
     vegburger.classList.toggle('fi-arrow-left');
 })
 
-vegburger.addEventListener('mouseleave',(event)=>{
+nav.addEventListener('mouseleave',(event)=>{
     nav.classList.remove('menu-active');
     vegburger.classList.remove('fi-arrow-left');
     vegburger.classList.add('fi-align-justify');
    
+})
+
+const products = [
+    {id:1,name:'malna', picture:'malna.jpg',
+        description:'Kézzel szedett finimság',
+        price:3800,instock:true},
+    {id:2,name:'Áfonya', picture:'afonya.jpg',
+        description:'Kézzel szedett finimság',
+        price:3250,instock:true},
+    {id:3,name:'Szeder', picture:'szeder.jpg',
+        description:'Kézzel szedett finimság',
+        price:1700,instock:true,variations:["feher","fekete"]}
+];
+
+const productSection = document.getElementById('products');
+
+products.forEach(product => {
+    productSection.innerHTML += `<div>
+        <h2>${product.name}</h2>
+        <p>${product.description}</p>
+        <image src="./image/${product.picture}">
+        <h3>${product.price}</h3>
+        <a id=${product.id} class="addToCart">Kosárba</a>
+        </div>`;
 })
