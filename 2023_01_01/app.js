edd22adde9cce481fdd59306c882e8e0663160db
szilasii <szilasii78@gmail.com>
2023-01-31 13:03:49 +0100

    fetch

--- 2023_01_01/app.js
+++ 2023_01_01/app.js
@@ -13,7 +13,7 @@ nav.addEventListener('mouseleave',(event)=>{
     vegburger.classList.add('fi-align-justify');
    
 })
-
+/*
 const products = [
     {id:1,name:'Málna', picture:'malna.jpg',
         description:'Kézzel szedett finimság',
@@ -25,26 +25,82 @@ const products = [
         description:'Kézzel szedett finimság',
         price:1700,instock:true,variations:["feher","fekete"]}
 ];
+*/
+let products = [];
+
+
+const addToCart = (event) => {
+    let target = event.target.id ? event.target.id : event.target.dataset.id;
+    cart[target] == undefined ? cart[target] = 1 : cart[target]++
+   }
+
+const refreshCartItems = () => {
+    cartItems.innerHTML = "";
+    let total = 0;
+    for(const id in cart) {
+        const currentProduct = products.find(product => product.id ==id);
+        cartItems.innerHTML += `<li>
+        <button data-id="${currentProduct.id}">+</button>
+        ${cart[id]} - db ${currentProduct.name} * ${currentProduct.price} Ft/db</li>`;
+        total += cart[id]*currentProduct.price;
+    }
+    cartItems.innerHTML += `<li>Összesen: ${total.toLocaleString()} Ft</li>`;
+}   
+
 
 const productSection = document.getElementById('products');
 
+fetch("https://hur.webmania.cc/products.json").then(response=>response.json()).then(data => {
+products = data.products;
+products.forEach(product => {
+    productSection.innerHTML += `<div>
+        <h2>${product.name}</h2>
+        <p>${product.description}</p>
+        <image src="${product.picture}">
+        <h3>${product.price} Ft</h3>
+        <a id=${product.id} class="addToCart">Kosárba</a>
+        </div>`;
+        addToChartButtons = document.getElementsByClassName('addToCart');
+        for (const addToChartButton of addToChartButtons) {
+            addToChartButton.addEventListener('click', addToCart);
+        }
+});
+
+
+}).catch(error=>console.log(error));
+
+
+/*
 products.forEach(product => {
     productSection.innerHTML += `<div>
         <h2>${product.name}</h2>
         <p>${product.description}</p>
         <image src="./img/${product.picture}">
-        <h3>${product.price}</h3>
+        <h3>${product.price} Ft</h3>
         <a id=${product.id} class="addToCart">Kosárba</a>
         </div>`;
-})
+        addToChartButtons = document.getElementsByClassName('addToCart');
+        for (const addToChartButton of addToChartButtons) {
+            addToChartButton.addEventListener('click', addToCart);
+        }
 
+})
+*/
 const cart = {}
 
-const cartIcon = document.getElementById('cart-icon')
-const cartContent = document.getElementById('cart-content')
-const cartItems = document.getElementById('cart-items')
+const cartIcon = document.getElementById('cart-icon');
+const cartContent = document.getElementById('cart-content');
+const cartItems = document.getElementById('cart-items');
+
 
 cartIcon.addEventListener('click', (event) => {
-    cartContent.classList.toggle('active')
+    cartContent.classList.toggle('active');
+    refreshCartItems();
+
 })
+
+cartItems.addEventListener('click',(event) => {
+    addToCart(event);
+    refreshCartItems();
+});
 
